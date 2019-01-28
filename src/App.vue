e1e28b192ce88ee799cd9dadda29d69c12076528
enderjj <chnhongjj@163.com>
2019-01-28 17:04:04 +0800

    feat: border 1px

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-items">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -26,12 +26,15 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import './common/stylus/mixin.styl'
+
 #app {
   .tab {
     display: flex;
     width: 100%;
     height: 40px;
     line-height: 40px;
+    border-1px(rgba(7, 17, 27, 0.1));
 
     .tab-items {
       flex: 1;
@@ -41,8 +44,9 @@ export default {
         display: block;
         font-size: 14px;
         color: rgb(77, 85, 93);
+
         &.active {
-          color: rgb(240,20,20);
+          color: rgb(240, 20, 20);
         }
       }
     }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue';
 import router from './router';
 import App from './App';
 
+import './common/stylus/index.styl';
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
